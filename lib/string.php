a9ef5fd117078eccb210a18d2aa7da775aabca46
kong0107 <kong0107@gmail.com>
2024-07-01 20:43:02 +0800

    add function `rebuild_url`

--- lib/string.php
+++ lib/string.php
@@ -31,7 +31,7 @@ function jwt_decode(
 function parse_dataurl(
     string $url
 ) : array {
-    if(preg_match('/^data:(\w+)\/([\w\.\-]+);base64,/', $url, $matches)) {
+    if (preg_match('/^data:(\w+)\/([\w\.\-]+);base64,/', $url, $matches)) {
         return array(
             'type' => $matchs[0],
             'subtype' => $matches[1],
@@ -46,3 +46,28 @@ function hsc(
 ) /*: mixed*/ {
     return is_string($str) ? htmlspecialchars($str) : $str;
 }
+
+
+/**
+ * Modify the given URL to which some query params are re-assigned.
+ * @example
+ * // returns 'foo.php?sort=date&page=3'
+ * rebuild_url(['sort' => 'date'], 'foo.php?sort=name&page=3');
+ */
+function rebuild_url(
+    array $new_params,
+    string $url = ''
+) : string {
+    if (! $url) $url = $_SERVER['REQUEST_URI'];
+    $parts = parse_url($url);
+    if (! $parts) throw new InvalidArgumentException('The second argument must be a URL.');
+
+    $pos = str_pos($url, '?');
+    if ($pos !== false) $url = substr($url, 0, $pos);
+
+    parse_str($parts['query'] ?? '', $old_params);
+    $params = array_merge($old_params, $new_params);
+
+    return $url .= '?' . http_build_query($params);
+}
+
